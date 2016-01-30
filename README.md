89620ea646a4ad3b35c1993e8216b622d7853e26
Nia Tomonaka <nia1012-tmnk@outlook.jp>
2016-01-30 23:33:12 +0900

    XSPADAのAndroid上でのテスト完了

--- README.md
+++ README.md
@@ -3,19 +3,27 @@
 ---
 
 名称 : すぱーダ（SPADA）  
-バージョン : Ver. 1.0.0 ( Dec. 27, 2015 )  
+バージョン : Ver. 1.0.5 ( Jan. 30, 2016 )  
   
 種別 : .NET クラスライブラリ  
-ターゲットバージョン : .NET Framework 4.0  
-ターゲットでの必要なランタイム : .NET Framework 4.0以上
+  
+ターゲットプラットフォーム :  
+* **すぱーダ（SPADA）** : .NET Framework 4.0  
+* **エクすぱーダ（XSPADA）** : .NET Framework 4.5、Xamarin.Android、Xamarin.iOS、Windows 8.x用ストアアプリ、Windows Phone 8.1（Silverlightには非対応）   
+* **ユニすぱーダ（UniSPADA）** : ユニバーサルWindows（Build 10240以上） 
   
 作成者 : 智中 ニア（ [@nia_tn1012](https://twitter.com/nia_tn1012/) ）  
 ライセンス : MIT Licence  
+  
 リンク  
 　http://chronoir.net （ホームページ）  
 　http://chronoir.net/spada （ライブラリのページ）  
 　https://github.com/Nia-TN1012/SPADA （GitHubのリポジトリ）  
-　https://www.nuget.org/packages/Chronir_net.SPADA/ （NuGet Gallery）
+  
+NuGet Gallery  
+* すぱーダ（SPADA）: https://www.nuget.org/packages/Chronir_net.SPADA/
+* エクすぱーダ（XSPADA）: https://www.nuget.org/packages/Chronoir_net.XSPADA/
+* ユニすぱーダ（UniSPADA）: https://www.nuget.org/packages/Chronoir_net.UniSPADA/ 
 
 ---
 
@@ -28,9 +36,11 @@
 ##◆ ダウンロードとインストール方法
 
 以下のリンクからzipファイルをダウンロードし、解凍します。  
-[Chronoir_net.SPADA.zip](http://chronoir.net/wp-content/uploads/Apps/Libraries/Chronoir_net.SPADA.zip)  
+* [Chronoir_net.SPADA.zip](http://chronoir.net/wp-content/uploads/Apps/Libraries/Chronoir_net.SPADA.zip)
+* [Chronoir_net.XSPADA.zip](http://chronoir.net/wp-content/uploads/Apps/Libraries/Chronoir_net.XSPADA.zip)
+* [Chronoir_net.UniSPADA.zip](http://chronoir.net/wp-content/uploads/Apps/Libraries/Chronoir_net.UniSPADA.zip)
 
-**Chronir_net.SPADA.dll**をアセンブリ参照に追加するか、**Chronir_net.SPADA.cs**をプロジェクトに追加します。
+DLLファイルをアセンブリ参照に追加するか、プロジェクトに追加します。
 
 ##◆ 使用方法
 
@@ -39,7 +49,8 @@
 すぱーダは すぱこーRSSフィードを読み込む`SpacoRSSReader`クラスと
 すぱこーの各話データを格納する`SpacoRSSItem`クラスの2つで構成されています。
 
-いずれも名前空間は`Chronoir_net.SPADA`です。
+いずれも名前空間は`Chronoir_net.SPADA`です。  
+※エクすぱーダは`Chronoir_net.XSPADA`、ユニすぱーダは`Chronoir_net.UniSPADA`です。
 
 ```csharp
 using Chronoir_net.SPADA;
@@ -50,7 +61,7 @@ using Chronoir_net.SPADA;
 すぱこーRSSフィードのURLを指定して読み込みます（内部でLINQ to XMLを用いています）。
 
 または、`SpacoRSSReader.Load( XmlReader )`メソッドで、
-XmlReaderオブジェクトを指定して読み込むことも可能です（こちらの方が高速です）。
+XmlReaderオブジェクトを指定して読み込むことも可能です（こちらの方が高速です）。  
 
 読み込みに成功すると、コンテンツのデータを格納した`SpacoRSSReader`オブジェクトが返ります。
 
@@ -69,6 +80,23 @@ using( XmlReader xr = XmlReader.Create( "http://pronama-api.azurewebsites.net/fe
 	...
 }
 ```
+
+エクすぱーダ及びユニすぱーダでは、非同期で読み込むための`SpacoRSSReaderAsync.Load( string, CancellationToken? )`メソッドと`SpacoRSSReader.LoadAsync( XmlReader, CancellationToken? )`メソッドを
+用意しています。また、WebからXmlReaderオブジェクトを生成する`SpacoRSSClient.GetXmlReaderAsync( string, CancellationToken? )`メソッドも用意しています。  
+これらのメソッドは、CancellationTokenを使って処理を中止させることができます。
+
+```csharp
+using System.Threading;
+using System.Xml;
+
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+using( XmlReader reader = await Task.Run( () => SpacoRSSClient.GetXmlReaderAsync( "http://pronama-api.azurewebsites.net/feed/spaco", cancellationTokenSource.Token ) ) ) {
+	SpacoRSSReader srr = await Task.Run( () => SpacoRSSReader.LoadAsync( reader, cancellationTokenSource.Token ) );
+	...
+}
+```
+
 
 ##◆ データの取り出し
 
@@ -171,7 +199,9 @@ class Program {
 
 
 ##◆ リリースノート
+* Ver. 1.0.5 : 2016/01/30  
+　 - すぱーダの動作改善、エクすぱーダとユニすぱーダを追加
 
 * Ver. 1.0.0 : 2015/12/27  
-　 - CNR-00000 : 初版リリース
+　 - 初版リリース
 
---
+++ XSPADA_Test/XSPADA_Test/ViewModel/RSSViewModel.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+using Chronoir_net.XSPADA;
+
+namespace XSPADA_Test {
+	// RSSリーダーのViewModelです。
+	class RSSViewModel : INotifyPropertyChanged {
+
+		// Model
+		RSSModel rssModel = new RSSModel();
+
+		// コンストラクター
+		public RSSViewModel() {
+			rssModel.PropertyChanged += ( sender, e ) =>
+				PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( e.PropertyName ) );
+			// RSSフィードの取得が完了したことをView側に通知します。
+			rssModel.GetRSSCompleted += ( sender, e ) => {
+				// RSSフィード取得中のフラグをオフにします。
+				IsProgress = false;
+				// RSSフィード取得完了したことをView側に通知します。
+				GetRSSCompleted?.Invoke( this, e );
+			};
+		}
+
+		// RSSフィードのタイトル
+		public string Title =>
+			IsProgress ? "すぱこーRSSフィードを取得中 ..." : rssModel.RSSProvider.Title;
+
+		// RSSフィードの説明
+		public string Description =>
+			IsProgress ? "Recieving ..." : rssModel.RSSProvider.Description;
+
+		// RSSフィードの最終更新日
+		public DateTime LastUpdatedTime =>
+			IsProgress ? DateTime.MinValue : rssModel.RSSProvider.LastUpdatedTime;
+
+		public string BannerURL =>
+			IsProgress ? "" : rssModel.RSSProvider.BannerURL;
+
+		// RSSフィードのコンテンツ
+		public ObservableCollection<SpacoRSSContent> Items => rssModel.Items;
+
+		// RSSフィードのURL
+		public string Url {
+			get { return rssModel.Url; }
+			set { rssModel.Url = value; }
+		}
+
+		// RSSフィード取得中のフラグ
+		private bool isProgress = false;
+		public bool IsProgress {
+			get { return isProgress; }
+			set {
+				isProgress = value;
+				NotifyPropertyChanged();
+				NotifyPropertyChanged( nameof( Title ) );
+				NotifyPropertyChanged( nameof( Description ) );
+				NotifyPropertyChanged( nameof( LastUpdatedTime ) );
+				NotifyPropertyChanged( nameof( BannerURL ) );
+			}
+		}
+
+		// RSSの取得完了後に発生させるイベントハンドラーです。
+		public event TaskResultEventHandler GetRSSCompleted;
+
+		// プロパティ変更後に発生させるイベントハンドラーです。
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		// プロパティ変更を通知します。
+		private void NotifyPropertyChanged( [CallerMemberName]string propertyName = null ) {
+			PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+		}
+
+		private ICommand getRSS;
+		public ICommand GetRSS => getRSS ?? ( getRSS = new GetRSSCommand( this ) );
+
+		private ICommand cancelGetRSS;
+		public ICommand CancelGetRSS => cancelGetRSS ?? ( cancelGetRSS = new CancelGetRSSCommand( this ) );
+
+		// RSSフィードを取得するコマンドです。
+		private class GetRSSCommand : ICommand {
+
+			// ViewModelの参照
+			private RSSViewModel rssViewModel;
+
+			//　コンストラクター
+			public GetRSSCommand( RSSViewModel viewModel ) {
+				rssViewModel = viewModel;
+				// コマンド実行の可否の変更を通知します。
+				rssViewModel.PropertyChanged += ( sender, e ) =>
+					CanExecuteChanged?.Invoke( sender, e );
+			}
+
+			// コマンドを実行できるかどうかを取得します。
+			public bool CanExecute( object parameter ) =>
+				!rssViewModel.IsProgress;
+
+			// コマンド実行の可否の変更した時のイベントハンドラーです。
+			public event EventHandler CanExecuteChanged;
+
+			// コマンドを実行し、RSSフィードを取得します。
+			public void Execute( object parameter ) {
+				// RSSフィード取得中のフラグをオンにします。
+				rssViewModel.IsProgress = true;
+				// RSSフィード取得します。
+				rssViewModel.rssModel.GetRSS();
+			}
+
+		}
+
+		// RSSフィードを取得を中止するコマンドです。
+		private class CancelGetRSSCommand : ICommand {
+
+			// ViewModelの参照
+			private RSSViewModel rssViewModel;
+
+			//　コンストラクター
+			public CancelGetRSSCommand( RSSViewModel viewModel ) {
+				rssViewModel = viewModel;
+				// コマンド実行の可否の変更を通知します。
+				rssViewModel.PropertyChanged += ( sender, e ) =>
+					CanExecuteChanged?.Invoke( sender, e );
+			}
+
+			// コマンドを実行できるかどうかを取得します。
+			public bool CanExecute( object parameter ) =>
+				rssViewModel.IsProgress;
+
+			// コマンド実行の可否の変更した時のイベントハンドラーです。
+			public event EventHandler CanExecuteChanged;
+
+			// コマンドを実行し、RSSフィードを取得します。
+			public void Execute( object parameter ) {
+				// RSSフィード取得を中止します。
+				rssViewModel.rssModel.CancelGetRSS();
+				// RSSフィード取得中のフラグをオフにします。
+				rssViewModel.IsProgress = false;
+			}
+
+		}
+
+	}
+}
